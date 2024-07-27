e34c14e5cb4e07852e60d545de3bdb4cc9d7e216
zuhal amin <zuhal.amin2017@gmail.com>
2024-07-27 21:00:34 +1000

    this is my second file

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print('Hi')
+print('Hello everyone this is mt first program')
+
+name = 'zuhal'
+age = 23
+print(name)
+print(age)
+#print (input('Enter your name:'))
---
+++ week1.py
@@ -0,0 +1,8 @@
+print('This is our new file ')
+name ='zuhal'
+age = 23
+print(name)
+print(age)
+
+name = input('Enter your name:')
+print('your name is ' +name)
