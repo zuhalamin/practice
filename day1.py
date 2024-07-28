5d9bec25e0cd2f1ead9575cba60625ba5644f84e
zuhal amin <zuhal.amin2017@gmail.com>
2024-07-28 20:46:46 +1000

    This is today's work

--- day1.py
+++ day1.py
@@ -9,6 +9,56 @@ print(d)
 print(type(d))
 def sum(a ,b):
     return a+b
-print(sum(10,100))
-def print(a):
-    return a
+#print(sum(10,100))
+#def print(a):
+ #   return a
+age = 20
+if age >18:
+    print('allowd')
+elif age==18:
+    print('mybe')
+#elif age <18:
+#print('reject')
+else:
+    print('no match')
+
+#veribale 
+x = 5 
+y = 23
+print(x+y)
+
+x = 34
+y = 'book'
+print(x)
+print(y)
+
+x , y , z = 'blue','red','green'
+print(x,y,z) 
+print(x)
+print(y)
+print(z)
+
+a = b = d = 'computer'
+print(a,b,d)
+
+frutis = ["orenge","apple","banana"]
+x , y , z = frutis
+print(x)
+print(z)
+print(y)
+print(x,y,z)
+ 
+d = 'This'
+b = 'is'
+p = 'wonderful'
+print(d,b,p)
+print(d+b+p)
+
+s = "sweet"
+
+def myfun():
+    global m
+    m ='english'
+    print(m , s)
+myfun()
+print(s,m)
