7db141499a4057d4ff5b9159719b338b7f076187
mohib-one-five <mohib@one-five.com>
2024-08-01 10:29:32 +0200

    Added Task3

---
+++ tasks/task3.py
@@ -0,0 +1,26 @@
+# Task Three: Determine Age Group
+# Create a Python script that determines the age group of a person based on their age.
+# The script should classify the person into one of the following categories:
+
+# "Child" (0-12 years)
+# "Teen" (13-19 years)
+# "Adult" (20-64 years)
+# "Senior" (65 years and above)
+
+# Objective:
+# Encapsulate the logic for determining the age group into a method.
+# The script should prompt the user to enter their age and then use the method to print the appropriate age group.
+
+# Steps to Complete the Task:
+
+# Define a Method:
+# Create a method named determine_age_group that takes one parameter: age.
+# Inside the method, use if, elif, and else statements to determine the age group based on the given age.
+# User Input:
+# In the main part of the script, prompt the user to enter their age.
+# Call the Method:
+# Pass the user-provided age to the determine_age_group method to determine and print the age group.
+
+#########################
+# WRITE YOUR CODE BELOW #
+#########################
