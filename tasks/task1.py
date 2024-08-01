6f3d14d39d37ce331c66ce1d07fcd865d05aa006
zuhal amin <zuhal.amin2017@gmail.com>
2024-08-01 21:14:06 +1000

    task1

--- tasks/task1.py
+++ tasks/task1.py
@@ -13,3 +13,12 @@
 
 # Use if, elif, and else statements to handle the comparison.
 # Ensure the script correctly handles all possible cases.
+num1=input('Please enter your number:')
+num2=input('please enter yoyr number:')
+if num1>num2:
+    print('The first number is greater than the second number.')
+elif num1<num2:
+    print('The first number is less than the second number.')
+elif num1==num2:
+    print('Both numbers are equal.')
+
