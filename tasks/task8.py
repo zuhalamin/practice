58b295f307117ca15ee84b6bfe2867cd7b28e2e1
mohibullah70 <mohibullah70@gmail.com>
2024-08-03 15:02:43 +0200

    Added Task8

---
+++ tasks/task8.py
@@ -0,0 +1,27 @@
+# Task: Vowel Counter
+# Objective:
+
+# Write a program that counts the number of vowels in a given string.
+# The program should ignore case, meaning it should count both uppercase and lowercase vowels.
+
+# Instructions:
+
+# Prompt the user to enter a string.
+# Convert the string to lowercase to handle case insensitivity.
+# Count the number of vowels (a, e, i, o, u) in the string.
+# Print the total number of vowels.
+# ===============================================================================================
+# Example:
+# Enter a string: Hello World
+# Number of vowels: 3
+
+# Enter a string: Python is Amazing
+# Number of vowels: 5
+# ================================================================================================
+# Hint:
+# Use the lower() method to convert the string to lowercase.
+# Use a loop or a comprehension to iterate through the characters and count the vowels.
+
+# ================================================================================================
+# WRITE YOUR CODE BELOW #
+# ================================================================================================
